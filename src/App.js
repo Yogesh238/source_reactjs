55eff0d0c7a677d98926817d6d3dd6324cdfd3ca
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-10 14:26:28 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>W3Adda - Hello Wolrd!</h1>
+      </div>
+    );
+  }
+}
+ 
+export default App;
