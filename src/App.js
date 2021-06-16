014aa5235843ebd1ff1df5a03be7ce7ad0cc8475
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-16 15:10:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>W3Adda - Hello Wolrd!</h1>
+        <h1>Hello World!</h1>
       </div>
     );
   }
