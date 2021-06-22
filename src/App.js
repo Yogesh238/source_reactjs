972169b51b5e43097d8b3549246992ee2c82816f
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2021-06-22 13:48:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello India from reactjs Application from Stage env</h1>
+        <h1>Hello India from reactjs Application from stage branch 1</h1>
       </div>
     );
   }
