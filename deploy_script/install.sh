9be7b78d343982b00033bdd64a1d1ab80c725297
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2022-01-13 12:43:18 +0530

    Update install.sh

--- deploy_script/install.sh
+++ deploy_script/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo systemctl stop nginx.service
 sudo rm -rf /var/www/html/*
-sudo mv -f /opt/codedeploy-agent/deployment-root/f98b0c15-66ab-4301-8c20-857b3311f064/${DEPLOYMENT_ID}/public/index.html /var/www/html/
+sudo mv -f /opt/codedeploy-agent/deployment-root/fb468ff7-0a6e-40b7-b4e0-dacbb73dffa5/${DEPLOYMENT_ID}/deployment-archive/public/index.html /var/www/html/
 sudo systemctl start nginx.service
