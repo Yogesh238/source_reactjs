8e4de01b5b01b95149a2c68bf2d943e992a729ac
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2022-01-20 15:39:49 +0530

    Update codedeploy.sh

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tag=`aws s3api head-object --bucket kt-session-yogesh-sachin --key KT_Demo/demo.zip --query ETag --output=text`
 echo $tag
-aws deploy create-deployment --application-name KT_Demo --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name KT_Demo_dg --description "deployment" --s3-location bucket=kt-session-yogesh-sachin,bundleType=zip,eTag=${tag},key=KT_Demo/demo.zip
+aws deploy create-deployment --application-name KT_Demo --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name KT_Demo_dg --description "deployment" --s3-location bucket=kt-session-yogesh-sachin,bundleType=zip,eTag='7133a9fbd1eebc24bf30028c77ef479f',key=KT_Demo/demo.zip
